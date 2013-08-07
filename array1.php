0569f29ce23ce8e5441be7f081e58aff24c235ad
JHerz <mokushface@gmail.com>
2013-08-06 22:57:20 -0400

    just added line breaks

--- array1.php
+++ array1.php
@@ -1,11 +1,11 @@
 <?php
-$record = array('first_nm' =>'Oscar', 'last_nm' =>'The_Grouch');
+$record = array('first_nm' =>'Oscar', 'last_nm' =>'The_Grouch</br>');
 $records[] = $record;
 $record['first_nm'] = 'Cookie';
-$record['last_nm'] = 'Monster';
+$record['last_nm'] = 'Monster</br>';
 $records[] = $record;
 $record['first_nm'] = 'Big';
-$record['last_nm'] = 'Bird';
+$record['last_nm'] = 'Bird</br>';
 $records[] = $record;
 print_r($records);
 ?>
