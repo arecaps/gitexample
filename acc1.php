1358ae14fecf27f3c750d13674d38155cf50eaf0
JHerz <mokushface@gmail.com>
2013-08-08 17:12:11 -0400

    account example from class

---
+++ acc1.php
@@ -0,0 +1,28 @@
+<?php
+  
+  $obj = new account(1000);
+  $obj->debit(100);
+  $obj->credit(200);
+  $obj->debit(500);
+  class account {
+    
+    public $starting_bal;
+    public $current_bal;
+
+    public function __construct($amount) {
+      $this->starting_bal = $amount;
+      $this->current_bal = $amount;
+      }
+    public function debit($amount){
+      
+      $this->current_bal = $this->current_bal - $amount;
+      }
+    public function credit($amount){
+      $this->current_bal = $this->current_bal + $amount;
+    }
+    public function __destruct() {
+      echo 'Your starting balance was: ' . $this->starting_bal . '<br>';
+      echo 'Your current balance is: ' . $this->current_bal . '<br>';
+    }
+  }
+?>
