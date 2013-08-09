beb8f746ab1fdb909f2ca6153d68898cf66b3c4b
JHerz <mokushface@gmail.com>
2013-08-09 18:09:06 -0400

    changed it to a foreach loop

--- array1.php
+++ array1.php
@@ -1,16 +1,6 @@
 <?php
-$record = array('first_nm' =>'Oscar', 'last_nm' =>'The_Grouch</br>');
-$records[] = $record;
-$record['first_nm'] = 'Cookie';
-$record['last_nm'] = 'Monster</br>';
-$records[] = $record;
-$record['first_nm'] = 'Big';
-$record['last_nm'] = 'Bird</br>';
-$records[] = $record;
-$record['first_nm'] = 'The';
-$record['last_nm'] = 'Count';
-$records[] = $record;
-sort($records);
-array_pop($records);
-print_r($records);
+$record = array('Oscar' =>' the Grouch</br>', 'Big' => 'Bird<br>','The' =>'Count');
+foreach ($record as $fir =>$las){
+echo "$fir   $las";
+}
 ?>
