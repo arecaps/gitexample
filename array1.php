35c726b174512ca5c157cc33772ff0a54fafbd4f
JHerz <mokushface@gmail.com>
2013-08-12 16:51:19 -0400

    Update array1.php

--- array1.php
+++ array1.php
@@ -1,5 +1,5 @@
 <?php
-$record = array('Oscar' =>' the Grouch</br>', 'Big' => 'Bird<br>','The' =>'Count<br>', 'Cookie' =>'Monster<br>');
+$record = array('Oscar' =>' the Grouch</br>', 'Big' => 'Bird<br>','The' =>'Count<br>', 'Cookie' =>'Monster<br>', 'Mr.' =>'Cooper<br>'););
 foreach ($record as $fir =>$las){
 echo "$fir   $las";
 }
