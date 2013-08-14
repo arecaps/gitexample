c5d485862bdbbc140b390e9ac0deedafd92c6160
JHerz <mokushface@gmail.com>
2013-08-13 21:52:00 -0400

    Update array1.php

--- array1.php
+++ array1.php
@@ -2,5 +2,7 @@
 $record = array('Oscar' =>' the Grouch</br>', 'Big' => 'Bird<br>','The' =>'Count<br>', 'Cookie' =>'Monster<br>', 'Mr.' =>'Cooper<br>'););
 foreach ($record as $fir =>$las){
 echo "$fir   $las";
+$last_only = array_values($record);
+print_r($last_only);
 }
 ?>
