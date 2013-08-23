79ef1c9dede8de0a9bc9a660c73e48e076092332
JHerz <mokushface@gmail.com>
2013-08-22 20:55:19 -0400

    fix

---
+++ array2.php
@@ -0,0 +1,17 @@
+<?php
+
+$boobledorf = array('boobles' , 'scaboodles' ,'snoodles' , 'krivitaboodles');
+
+sort($boobledorf);
+
+count($boobledorf);
+
+ // if (array_key_exists('snoodles', $boobledorf)){
+    foreach ($boobledorf as $creatures){
+    echo "the $creatures are denizens of Boobledorf<br>";
+    }
+ // }
+
+
+
+?>
