63f76c4726942c96ec819481a8937c60b28912ad
JHerz <mokushface@gmail.com>
2013-08-27 01:15:54 -0400

    funct override

---
+++ funct.php
@@ -0,0 +1,11 @@
+<?php
+
+function food($milchig = "pizza", $fleishig = "chicken soup"){
+  echo "$milchig is milchig, but $fleishig is fleishig";
+  } 
+
+echo food();
+
+echo food("cheesecake","filet mignon");
+
+?>
