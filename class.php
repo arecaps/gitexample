8b4546d4bc7231a3ea5b70e3dce2b9ca76f17400
JHerz <mokushface@gmail.com>
2013-08-31 22:59:48 -0400

    some minor changes

--- class.php
+++ class.php
@@ -1,40 +1,44 @@
 <?php
+
 $obj = new program();
 
 class program {
-public function __construct() {	
-if(isset($_REQUEST['class'])) {
-$class = $_REQUEST['class'];
-$obj = new $class();
-} else {	
-$obj = new homepage();
+  public function __construct() {	
+    if(isset($_REQUEST['class'])) {
+      
+      $class = $_REQUEST['class'];
+      $obj = new $class();
+      } else {	
+
+          $obj = new homepage();
+          }
+  print_r($_REQUEST);
+  }
 }
-print_r($_REQUEST);
-}
-}
+
 class page {	
-public function __construct() {
-if($_SERVER['REQUEST_METHOD'] == 'GET') {
-$this->get();
-} else {
-$this->post();
-}	
+
+  public function __construct() {
+    if($_SERVER['REQUEST_METHOD'] == 'GET') {
+      $this->get();
+      } else {
+        $this->post();
+        }	
+  
 }
-protected function get() {
-echo '<a href="class.php?class=sign_up">Sign up</a>' . "<br> \n";
-echo '<a href="class.php?class=login">Log in</a>' . "<br> \n";	
-}
-protected function post() {
-print_r($_POST);
-}
+  protected function get() {
+    echo '<a href="class.php?class=sign_up">Sign up</a>' . "<br> \n";
+    echo '<a href="class.php?class=login">Log in</a>' . "<br> \n";	
+  }
+
+  protected function post() {
+  print_r($_POST);
+  }
 }
 class sign_up extends page {
-public function get() {
-//echo '<a href="class.php?class=form1">Sign up</a>' . "<br> \n";
-//echo '<a href="class.php?class=form2">Log in</a>' . "<br> \n";
-//echo '<a href="class.php">Homepage</a>' . "<br> \n";
+  public function get() {
 
-$form = '<FORM action="class.php?class=sign_up" method="post">
+  $form = '<FORM action="class.php?class=sign_up" method="post">
 <P>
 <LABEL for="firstname">First name: </LABEL>
 <INPUT type="text" name="firstname" id="firstname"><BR>
@@ -50,13 +54,14 @@ $form = '<FORM action="class.php?class=sign_up" method="post">
 </P>
 </FORM>';
 
-echo $form;
+  echo $form;
 
-}	
+  }	
 
 }
+
 class login extends page {
-public function __construct() {
+  public function __construct() {
 	$form = '<FORM action="class.php?class=login" method="post">
 	<P>
 	<LABEL for="username">Username: </LABEL>
@@ -68,7 +73,8 @@ public function __construct() {
 	</FORM>';
 	
 	echo $form;
+  }
 }
-}
+
 class homepage extends page {}
 ?>
