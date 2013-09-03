8c6091c67e209a333c18a3572ed02a82a2a663a4
JHerz <mokushface@gmail.com>
2013-09-03 09:46:21 -0400

    improved margins

--- class.php
+++ class.php
@@ -11,12 +11,11 @@ class program {
       } else {	
 
           $obj = new homepage();
-          }
-  }
+        }
+    }
 }
 
 class page {	
-
   public function __construct() {
     if($_SERVER['REQUEST_METHOD'] == 'GET') {
       $this->get();
@@ -24,37 +23,39 @@ class page {
         $this->post();
         }	
   
-}
+    }
   protected function get() {
     echo '<a href="class.php?class=sign_up">Sign up</a>' . "<br> \n";
-    echo '<a href="class.php?class=login">Log in</a>' . "<br> \n";	
- }
+    echo '<a href="class.php?class=login">Log in</a>' . "<br> \n";	 
+	}
 }
+
 class sign_up extends page {
   public function get() {
 
   $form = '<FORM action="class.php?class=sign_up" method="post">
-<P>
-<LABEL for="firstname">First name: </LABEL>
-<INPUT type="text" name="firstname" id="firstname"><BR>
-<LABEL for="lastname">Last name: </LABEL>
-<INPUT type="text" name="lastname" id="lastname"><BR>
-<LABEL for="email">email: </LABEL>
-<INPUT type="text" name="email"id="email"><BR>
-<LABEL for="username">Username: </LABEL>
-<INPUT type="text" name="username" id="username"><BR>		
-<LABEL for="password">Password: </LABEL>
-<INPUT type="text" name="password" id="password"><BR>
-<INPUT type="submit" value="Send"> 
-</P>
-</FORM>';
+	<P>
+	<LABEL for="firstname">First name: </LABEL>
+	<INPUT type="text" name="firstname" id="firstname"><BR>
+	<LABEL for="lastname">Last name: </LABEL>
+	<INPUT type="text" name="lastname" id="lastname"><BR>
+	<LABEL for="email    ">email: </LABEL>
+	<INPUT type="text" name="email"id="email"><BR>
+	<LABEL for="username">Username: </LABEL>
+	<INPUT type="text" name="username" id="username"><BR>		
+	<LABEL for="password">Password: </LABEL>
+	<INPUT type="text" name="password" id="password"><BR>
+	<INPUT type="submit" value="Send"> 
+	</P>
+	</FORM>';
 
   echo $form;
   }	
-public function post(){
- $obj = new write_info;
+  public function post(){
+    $obj = new write_info;
+  }
 }
-}
+
 class login extends page {
   public function get() {
 	$form = '<FORM action="class.php?class=login" method="post">
@@ -69,10 +70,11 @@ class login extends page {
 	
 	echo $form;
   }
-public function post(){
-$obj = new authenticate;
-}
+
+  public function post(){
+    $obj = new authenticate;
   }
+}
 
 class homepage extends page {}
 
@@ -97,16 +99,18 @@ class write_info{
     fclose($user_file);
    }
    }
-    
+ //instantiate transactions class here   
 }   
+
 class authenticate{
-function __construct(){
-if ($_POST['username'] == null || $_POST['password'] == null){
-echo 'Error! Please enter both your username and your password in order to proceed<br>';
-echo login::get();
- }
+  function __construct(){
+    if ($_POST['username'] == null || $_POST['password'] == null){
+      echo 'Error! Please enter both your username and your password in order to proceed<br>';
+      echo login::get();
+    }
  //validate here
 //else
+ //after validation send it to transactions
  }
 }
 ?>
