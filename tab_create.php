b140f7b3e0825eb21fbbfb2d385d1c286f33df12
JHerz <mokushface@gmail.com>
2013-11-19 19:22:18 -0500

    made id the primary key

--- tab_create.php
+++ tab_create.php
@@ -13,7 +13,7 @@ echo $e->getMessage();
 $DBH->query('DROP TABLE IF EXISTS school_data.enrollment11'); 
 $DBH->query('CREATE DATABASE school_data');
 
-$DBH->query('CREATE TABLE school_data.enrollment11 (ID INT, enrollment INT)');
+$DBH->query('CREATE TABLE school_data.enrollment11 (ID INT PRIMARY KEY, enrollment INT)');
 
 $DBH = null;
 ?>
