9061ae4eb1df55bffcba42172a62f763e5c3c999
JHerz <mokushface@gmail.com>
2013-11-24 17:08:48 -0500

    added another table

--- tab_create.php
+++ tab_create.php
@@ -13,7 +13,7 @@ echo $e->getMessage();
 $DBH->query('DROP TABLE IF EXISTS school_data.enrollment11'); 
 $DBH->query('CREATE DATABASE school_data');
 
-$DBH->query('CREATE TABLE school_data.enrollment11 (ID INT PRIMARY KEY, enrollment INT)');
+$DBH->query('CREATE TABLE school_data.enrollment11 (ID INT PRIMARY KEY, name VARCHAR(60), enrollment INT)');
 
 $DBH = null;
 ?>
